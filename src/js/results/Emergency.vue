4b3a1b8336fc12b7d1d1e81cd643d82b33da7469
byjml <by@jml.so>
2020-04-14 23:16:21 +0300

    Update emergency number ☎😏

--- src/js/results/Emergency.vue
+++ src/js/results/Emergency.vue
@@ -2,7 +2,7 @@
     <div class="result">
       <div class="alert alert--danger" role="alert">
         <p class="alert__title">Natiijada iska-qiimaynta COVID-19</p>
-        <p class="alert__text">Fadlan wac 992 si aad u la hadasho kaaliye caafimaad.</p>
+        <p class="alert__text">Fadlan wac 449 si aad u la hadasho kaaliye caafimaad.</p>
       </div>
     </div>
 </template>
